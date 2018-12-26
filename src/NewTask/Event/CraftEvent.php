6d4b71d6ff099b80b134d6887f9d306accbef347
SmallasWater <46156675+SmallasWater@users.noreply.github.com>
2018-12-26 17:49:02 +0800

    Add files via upload

---
+++ src/NewTask/Event/CraftEvent.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+namespace NewTask\Event;
+
+
+use pocketmine\event\inventory\CraftItemEvent;
+use pocketmine\event\Listener;
+use pocketmine\level\sound\AnvilUseSound;
+use pocketmine\math\Vector3;
+use NewTask\TextMain;
+
+class CraftEvent implements Listener
+{
+    protected $main;
+    public function __construct(TextMain $main)
+    {
+        $this->main = $main;
+    }
+    //TODO 玩家合成物品
+    public function Craft(CraftItemEvent $e){
+        $player = $e->getPlayer();
+        $items = $e->getOutputs();
+        foreach ($items as $item){
+            $playerConfig = $this->main->getPlayerConfig($player)->getAll();
+            foreach ($playerConfig as $test => $value){
+                if($playerConfig[$test]["当前状态"] == false) continue;
+                $testArray = $this->main->getTestConfig($test)->getAll();
+                if($testArray["任务类型"] == "craft"){
+                    foreach ($value["Item"] as $name => $item_){
+                        $names = explode(':',$name);
+                        if($item->getId().":".$item->getDamage() == $names[0].":".$names[1]){
+                            $playerConfig[$test]["Item"][$name] += $item->getCount();
+                        }
+                    }
+                    $this->main->savePlayerConfig($player,$playerConfig);
+                    if($this->main->if_complete($player,$test)){
+                        $player->sendMessage("§e叮咚~~恭喜你完成了§b[§c".TextMain::getStar((int)$testArray["难度"]).
+                            "§b]§r§b[".$test."§b]§a任务~~§d快去领取奖励吧");
+                        $sound = new AnvilUseSound(new Vector3($player->x,$player->y,$player->z));
+                        $player->level->addSound($sound);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/NewTask/Event/JoinEvent.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+namespace NewTask\Event;
+
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use NewTask\TextMain;
+
+class JoinEvent implements Listener
+{
+    //TODO 玩家加入事件
+
+    protected $main;
+    public function __construct(TextMain $main)
+    {
+        $this->main = $main;
+    }
+    public function onJoin(PlayerJoinEvent $e){
+        $player = $e->getPlayer();
+        if(!$this->main->if_exitPlayerFile($player)){
+            Server::getInstance()->broadcastMessage(TextFormat::GREEN.$player->getName().TextFormat::WHITE."刚刚获得了".TextFormat::GREEN."开启任务时代!! 成就");
+            $this->main->getPlayerConfig($player);
+            $c = $this->main->getIntegralConfig();
+            $array = $c->getAll();
+            if(!isset($array[$player->getName()])){
+                $array[$player->getName()] = 0;
+                $c->setAll($array);
+                $c->save();
+            }
+        }
+    }
+}
